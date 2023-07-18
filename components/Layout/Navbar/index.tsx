66c15a7bc3353cf2cdbb83ec5c3e2f87f273867c
growtoups <67888122+growtoups@users.noreply.github.com>
2023-07-18 13:49:28 -0400

    Update index.tsx

--- components/Layout/Navbar/index.tsx
+++ components/Layout/Navbar/index.tsx
@@ -63,7 +63,8 @@ export const Navbar: FC = () => {
 		<>
 			<nav className="relative px-6 py-6 flex justify-between items-center bg-gray-900">
 				<div className="flex items-center justify-between">
-					<div> <img  src={icon}  alt="Logo"/>
+					<div className="flex items-center">
+						<img src={icon} alt="Logo" className="mr-2" />
 						<span className="text-2xl font-bold lg:text-3xl text-gray-300">
 							{CONFIG.OTHER.NAME}
 						</span>
